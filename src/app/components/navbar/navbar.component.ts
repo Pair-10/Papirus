226ade773b1efd8d393964f3abcba8220dbffa51
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-11 13:52:05 +0300

    navbar_added

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+  isHovered: boolean[] = [false, false, false];
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log(this.isMenuOpen)
+  }
+  onHover(index: number, isHover: boolean) {
+    this.isHovered[index] = isHover;
+  }
+}
