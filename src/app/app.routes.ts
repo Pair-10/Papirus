5e2a3b5148af5a7fd32a14117862b481c02d3529
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-14 23:51:16 +0300

    navbar_router_added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
-import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
-    {path:'', component:MainComponent}
+    {path:'', component:MainComponent},
 ];
