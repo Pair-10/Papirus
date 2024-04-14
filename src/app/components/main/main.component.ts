260ca2f0a101d8d3b5bb8585981a5d3dda250ddb
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-14 23:34:14 +0300

    activities_added

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HerosectionComponent } from '../herosection/herosection.component';
 import { HighlightsComponent } from '../highlights/highlights.component';
+import { ActiviesComponent } from '../activies/activies.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HerosectionComponent, HighlightsComponent],
+  imports: [HerosectionComponent, HighlightsComponent, ActiviesComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
