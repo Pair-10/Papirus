2009b2ac6a36660f1ead6bb0d170621f967b27cc
Özge Kestane <ozgekestane97@gmail.com>
2024-04-15 19:25:29 +0300

    created bookdetails pages - comments added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { Routes } from '@angular/router';
 import { MainComponent } from './shared/components/main/main.component';
+import { BookDetailComponent } from './features/book/pages/book-detail/book-detail.component';
 
 export const routes: Routes = [
     {path:'', component:MainComponent},
+    {path:'book-detail', component:BookDetailComponent},
 ];
