c43de6a7b222032eb377d9667eef343d9975ac46
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-18 16:29:32 +0300

    services_models_added_material-list_added_navbar_updated

---
+++ src/app/shared/components/material-list/material-list.component.ts
@@ -0,0 +1,80 @@
+import { Article } from './../../../models/article/article';
+import { Magazine } from './../../../models/magazine/magazine';
+import { Book } from './../../../models/book/book';
+import { ArticleService } from './../../../services/article/article.service';
+import { MagazineService } from './../../../services/magazine/magazine.service';
+import { BookService } from './../../../services/book/book.service';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
+@Component({
+    selector: 'app-material-list',
+    templateUrl: './material-list.component.html',
+    styleUrls: ['./material-list.component.css'],
+    standalone: true,
+    imports: [CommonModule]
+})
+export class MaterialListComponent implements OnInit {
+    selectedMaterialType: string = 'book';
+    books: Book[] = [];
+    magazines: Magazine[] = [];
+    articles: Article[] = [];
+    categories$: Observable<{ id: number; name: string }[]> = of([]);
+    // Seçilen kategori
+    selectedCategory: number = 0;
+
+    constructor(
+        private route: ActivatedRoute,
+        private bookService: BookService,
+        private magazineService: MagazineService,
+        private articleService: ArticleService
+    ) {
+        
+    }
+
+    ngOnInit() {
+        this.route.queryParams.subscribe(params => {
+            this.selectedMaterialType = params['type'] || 'book';
+            this.loadMaterials();
+            this.loadCategories();
+        });
+    }
+    loadCategories() {
+        // `selectedMaterialType` değiştiğinde kategorileri güncelleyin
+        this.categories$ = this.selectedMaterialType === 'book'
+            ? this.bookService.getCategories()
+            : this.selectedMaterialType === 'article'
+                ? this.articleService.getCategories()
+                : this.magazineService.getCategories();
+    }
+    selectCategory(categoryId: number) {
+        this.selectedCategory = categoryId;
+        this.loadMaterials(); // Seçilen kategoriye göre ürünleri yeniden yükleyin
+    }
+    loadMaterials() {
+        if (this.selectedMaterialType === 'book') {
+            this.bookService.getBooks().subscribe(books => {
+                // Kategoriyi filtreleyin
+                this.books = this.selectedCategory
+                    ? books.filter(book => book.categoryId === this.selectedCategory)
+                    : books;
+            });
+        } else if (this.selectedMaterialType === 'magazine') {
+            this.magazineService.getMagazines().subscribe(magazines => {
+                // Kategoriyi filtreleyin
+                this.magazines = this.selectedCategory
+                    ? magazines.filter(magazine => magazine.categoryId === this.selectedCategory)
+                    : magazines;
+            });
+        } else if (this.selectedMaterialType === 'article') {
+            this.articleService.getArticles().subscribe(articles => {
+                // Kategoriyi filtreleyin
+                this.articles = this.selectedCategory
+                    ? articles.filter(article => article.categoryId === this.selectedCategory)
+                    : articles;
+            });
+        }
+    }
+}
