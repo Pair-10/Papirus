4f51201fd75277e2615d49dbe062aeb02a540bdd
kader <kader.ergin.37@gmail.com>
2024-04-19 19:04:34 +0300

    Penalty-Admin_Page_Added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './shared/components/main/main.component';
 import { BookDetailComponent } from './features/book/pages/book-detail/book-detail.component';
 import { PenaltyComponent } from './features/penalty/pages/penalty/penalty.component'; //penalty
 import { MaterialListComponent } from './shared/components/material-list/material-list.component';
+import { PenaltyAdminComponent } from './features/penalty/pages/penalty-admin/penalty-admin.component'; //penalty-admin
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'book-detail', component: BookDetailComponent },
     { path: 'penalty', component: PenaltyComponent }, //penalty
-    { path: 'material-list', component: MaterialListComponent }
+    { path: 'material-list', component: MaterialListComponent },
+    { path: 'penalty-admin', component: PenaltyAdminComponent }, //penalty-admin
 ];
