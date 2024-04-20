d8e28f1e14afa01b2d92c369ccc36048b9fa7015
Huseyin Karakus <vanneohuseyin@gmail.com>
2024-04-20 14:26:49 +0300

    404notfoundpage_added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './shared/components/main/main.component';
 import { BookDetailComponent } from './features/book/pages/book-detail/book-detail.component';
 import { PenaltyComponent } from './features/penalty/pages/penalty/penalty.component'; //penalty
 import { MaterialListComponent } from './shared/components/material-list/material-list.component';
+import { NotfoundComponent } from './shared/components/notfound/notfound.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent },
     { path: 'book-detail', component: BookDetailComponent },
     { path: 'penalty', component: PenaltyComponent }, //penalty
-    { path: 'material-list', component: MaterialListComponent }
+    { path: 'material-list', component: MaterialListComponent },
+    { path: '**', component: NotfoundComponent }
 ];
