02656a39fca4c04b7d0fe119c772a4cccabd1988
Halil İbrahim Özcanlı <halilibrahimozcanli22@gmail.com>
2024-04-21 13:47:37 +0300

    login&register&forget-password&new-password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,17 @@ import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { MainComponent } from './shared/components/main/main.component';
 import { CommunicationService } from './services/communication.service';
+import { LoginComponent } from './shared/components/authentication/login/login.component';
+import { RegisterComponent } from './shared/components/authentication/register/register.component';
+import { ForgotPasswordComponent } from './shared/components/authentication/forgot-password/forgot-password.component';
+import { NewPasswordComponent } from './shared/components/authentication/new-password/new-password.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainComponent, FooterComponent, RouterOutlet, RouterLink],
+  imports: [RouterOutlet, NavbarComponent, MainComponent, FooterComponent, RouterOutlet, RouterLink, LoginComponent, RegisterComponent, ForgotPasswordComponent, NewPasswordComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { PenaltyComponent } from './features/penalty/pages/penalty/penalty.compo
 import { MaterialListComponent } from './shared/components/material-list/material-list.component';
 import { PenaltyAdminComponent } from './features/penalty/pages/penalty-admin/penalty-admin.component'; //penalty-admin
 import { ActivityComponent } from './features/activity/pages/activity/activity.component';//activity
+import { LoginComponent } from './shared/components/authentication/login/login.component';
+import { RegisterComponent } from './shared/components/authentication/register/register.component';
+import { ForgotPasswordComponent } from './shared/components/authentication/forgot-password/forgot-password.component';
+import { NewPasswordComponent } from './shared/components/authentication/new-password/new-password.component';
 import { NotfoundComponent } from './shared/components/notfound/notfound.component';
 
 
@@ -16,6 +20,10 @@ export const routes: Routes = [
     { path: 'material-list', component: MaterialListComponent },
     { path: 'penalty-admin', component: PenaltyAdminComponent }, //penalty-admin
     { path: 'activity', component: ActivityComponent }, //activity
+    { path: 'login',component:LoginComponent},
+    { path: 'register',component:RegisterComponent},
+    { path:'forgot-password',component:ForgotPasswordComponent},
+    { path:'new-password',component:NewPasswordComponent},
     { path: '**', component: NotfoundComponent }
 
 
